fe6658e2efb546419b7091ef71fce614457cd622
IllegalDoc <anis921787@gmail.com>
2025-04-01 16:06:33 +0100

    mostly done

--- scr.js
+++ scr.js
@@ -11,7 +11,7 @@ function multiply(a, b) {
 function divide(a, b) {
   if (b !== 0) {
     return a / b;
-  } else return "cant divide by 0";
+  } else return "لا اظن ان امك تحبك";
 }
 
 function operate(a, b, operator) {
@@ -23,27 +23,26 @@ function operate(a, b, operator) {
   } else if (operator === "*") {
     return multiply(a, b);
   } else if (operator === "/") {
-    return divide(a / b);
+    return divide(a, b);
   }
 }
 
 const NumberButtons = document.querySelectorAll("button.number");
 const DisplayDiv = document.querySelector("div.display");
+const DisplayContent = DisplayDiv.textContent;
 // THESE ARE THE VARIABLES NEEDED AS OF NOW
 let operator = "";
 let total = 0;
 let value1 = 0;
 let value2 = 0;
-let x = 0; // THIS VARIABLE IS MADE TO ALLOW AN TO OVERRIDE AN IF STATEMENT
+let x = 0;
 NumberButtons.forEach((number) => {
   number.addEventListener("click", (e) => {
     if (DisplayDiv.textContent.length < 10) {
       if (DisplayDiv.textContent === "0" || x !== 0) {
-        // THE X IS MADE IN CASE VALUE1 ONE IS INSERTED , X HELPS TO NOT CONTINUE WRITING ON THE FIRST VALUE1 WHILE INSERTING VALUE 2
         x = 0;
         DisplayDiv.textContent = number.textContent;
-      } else DisplayDiv.textContent += number.textContent; // WHEN X BECOMES 0, THE IF STATEMENT IS IGNORED AND IT COMES HERE,
-      //  THIS IS NEEDED TO TYPE NUMBERS AND NOT JUST ONE DIGIT
+      } else DisplayDiv.textContent += number.textContent;
     }
   });
 });
@@ -58,33 +57,63 @@ ResetButton.addEventListener("click", (e) => {
 const Operations = document.querySelectorAll("button.operation"); //FOUR OPERATION BUTTONS +-/*
 Operations.forEach((operation) => {
   operation.addEventListener("click", (e) => {
-    const operator = operation.textContent;
     if (value1 === 0) {
+      operator = operation.textContent;
       // THIS IS MADE IN CASE WE JUST STARTED, FIRST INPUT
-      value1 = parseInt(DisplayDiv.textContent);
+      value1 = Number(DisplayDiv.textContent);
       x = 1;
     } else {
       // THIS IS WHEN OPERATIONS START,
-      value2 = parseInt(DisplayDiv.textContent);
+      value2 = Number(DisplayDiv.textContent);
+
       total = operate(value1, value2, operator);
-      // I TAKE VALUE2 AND MAKE AN OPERATION
-      value1 = total; // THIS IS MADE BECAUSE THE FIRST IF STATEMENT WILL NOT BE
-      //TRIGERRED AGAIN AND TO SAVE THE total obv
+
+      console.log(value1);
+      console.log(value2);
+      value1 = total;
+      operator = operation.textContent;
     }
-    if (operator === "/" || "*") {
-      // I THOUGHT OF THIS IN CASE I CLICKED ON AN OPERATION FIRST , ITS BLURRY FOR ME TOO ,MAZAL MAFRITHASH HNA
-      value2 = 1;
-    } else value2 = 0;
 
     x = 1;
-    DisplayDiv.textContent = total;
 
-    console.log(value1);
+    DisplayDiv.textContent = value1;
   });
 });
 
-const Result = document.querySelector("button.equals"); //IGNORE THIS FOR NOW
+const Result = document.querySelector("button.equals");
 Result.addEventListener("click", (e) => {
-  AddupValue = parseInt(DisplayDiv.textContent);
-  DisplayDiv.textContent = operate(total, AddupValue, operator);
+  value2 = Number(DisplayDiv.textContent);
+  DisplayDiv.textContent = operate(value1, value2, operator);
+  value1 = operate(value1, value2, operator);
+});
+
+const floatButton = document.querySelector("button.float");
+floatButton.addEventListener("click", (e) => {
+  if (!DisplayDiv.textContent.includes(".")) {
+    DisplayDiv.textContent += ".";
+  }
+});
+
+const negativeButton = document.querySelector("button.negative");
+negativeButton.addEventListener("click", (e) => {
+  if (DisplayDiv.textContent !== "0") {
+    if (DisplayDiv.textContent.includes("-")) {
+      let newstr = DisplayDiv.textContent.slice(1);
+      DisplayDiv.textContent = newstr;
+    } else DisplayDiv.textContent = "-" + DisplayDiv.textContent;
+  }
+});
+
+const removeButton = document.querySelector("p.remove");
+removeButton.addEventListener("click", (e) => {
+  if (DisplayDiv.textContent.length > 1) {
+    let newstr = DisplayDiv.textContent.slice(0, -1);
+    DisplayDiv.textContent = newstr;
+  } else DisplayDiv.textContent = "0";
+});
+
+const percentageButton = document.querySelector("button.percentage");
+percentageButton.addEventListener("click", (e) => {
+  let percentage = Number(DisplayDiv.textContent) / 100;
+  DisplayDiv.textContent = percentage;
 });
